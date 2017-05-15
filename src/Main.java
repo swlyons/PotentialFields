39f337e9b59f35644a8b34281e1773ea89b1e355
--replace-all <PestytheRat@gmail.com>
2017-05-15 13:56:32 -0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.net.UnknownHostException;
+import java.util.*;
+import java.io.*;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String hostName = "This is the hotname";
+        int portNumber = 4444;
+
+        try {
+            Socket theSocket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(theSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(theSocket.getInputStream()));
+
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            String fromServer;
+            String fromUser;
+
+            while ((fromServer = in.readLine()) != null) {
+                System.out.println("Server: " + fromServer);
+                if (fromServer.equals("Bye."))
+                    break;
+
+                fromUser = stdIn.readLine();
+                if (fromUser != null) {
+                    System.out.println("Client: " + fromUser);
+                    out.println(fromUser);
+                }
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
+        }
+    }
+}
+}

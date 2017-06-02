5f6e151e5aa10a84d0221ae4ddad67474c155e4d
--replace-all <PestytheRat@gmail.com>
2017-06-01 18:10:10 -0600

    RRT WORKSgit add src/RRTcommand.java !

--- src/RRTcommand.java
+++ src/RRTcommand.java
@@ -9,6 +9,8 @@ public class RRTcommand {
     }
 
     public List<PotentialField> goToGoalRRT(int goal) {
+        System.out.println("START");
+        int obstacleSize= 130;
         int deltaQ = 70;
         int goalX = -1;
         int goalY = -1;
@@ -16,7 +18,8 @@ public class RRTcommand {
         String hostName = "192.168.2.29";
         int portNumber = 55555;
         List<int[]> rrtree = new ArrayList<int[]>();
-
+        List<Integer> cameFrom = new ArrayList<Integer>();
+        cameFrom.add(-1);
         List<Location> obstacleLocations = new ArrayList<Location>();
         try {
             Communicator c = new Communicator(hostName, portNumber);
@@ -40,9 +43,13 @@ public class RRTcommand {
             rrtree.get(0)[0] = (int) start.getX();
             rrtree.get(0)[1] = (int) start.getY();
 
+            System.out.println(start.getX());
+            System.out.println(goalX);
+
+            int iterations = 0;
             boolean notComplete = true;
             while (notComplete) {
-                int xRand = r.nextInt(1400);
+                int xRand = r.nextInt(1700);
                 int yRand = r.nextInt(800);
 
                 int nearest = 0;
@@ -75,15 +82,17 @@ public class RRTcommand {
                 boolean tooCloseToAnObstacle = false;
                 for (int i = 0; i < obstacleLocations.size(); i++) {
                     double xKnot = obstacleLocations.get(i).getX();
-                    double yKnot = obstacleLocations.get(i).getX();
+                    double yKnot = obstacleLocations.get(i).getY();
                     double xOne = rrtree.get(nearest)[0];
                     double yOne = rrtree.get(nearest)[1];
                     double xTwo = xNew;
                     double yTwo = yNew;
 
+                    distance = Math.max(distance, 1);
+
                     int obstacleDistance = (int) Math.abs((yTwo - yOne) * xKnot - (xTwo - xOne) * yKnot + xTwo * yOne - yTwo * xOne) / distance;
 
-                    if (obstacleDistance < 100) {
+                    if (obstacleDistance < obstacleSize) {
                         tooCloseToAnObstacle = true;
                         break;
                     }
@@ -93,26 +102,32 @@ public class RRTcommand {
                     rrtree.add(new int[2]);
                     rrtree.get(rrtree.size() - 1)[0] = xNew;
                     rrtree.get(rrtree.size() - 1)[1] = yNew;
+                    cameFrom.add(nearest);
                     //check to see if goal is in range
                     if ((goalX - xNew) * (goalX - xNew) + (goalY - yNew) * (goalY - yNew) < deltaQ * deltaQ) {
-                        rrtree.add(new int[2]);
-                        rrtree.get(rrtree.size() - 1)[0] = goalX;
-                        rrtree.get(rrtree.size() - 1)[1] = goalY;
-                        notComplete=false;
+//                        rrtree.add(new int[2]);
+//                        rrtree.get(rrtree.size() - 1)[0] = goalX;
+//                        rrtree.get(rrtree.size() - 1)[1] = goalY;
+
+                        notComplete = false;
                     }
                 }
             }
 
             List<PotentialField> pointsAsAnswers = new ArrayList<PotentialField>();
-            for(int i=0;i<rrtree.size();i++){
-                pointsAsAnswers.add(new AttractionField(rrtree.get(i)[0],rrtree.get(i)[1]));
+            pointsAsAnswers.add(new AttractionField(goalX, goalY));
+
+            for (int i = cameFrom.get(cameFrom.size() - 1); i != -1; i = cameFrom.get(i)) {
+                pointsAsAnswers.add(0, new AttractionField(rrtree.get(i)[0], rrtree.get(i)[1]));
             }
+            System.out.println("END");
             return pointsAsAnswers;
 
         } catch (Exception e) {
             System.out.println(e);
             System.out.println(e.getMessage());
-            return  null;
+            System.out.println("ERROR");
+            return null;
         }
 
     }
